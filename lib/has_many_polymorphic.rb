6a6ae097427901d6542aed9186304ac9a0be7505
russ1985 <russellfholmes@gmail.com>
2011-07-25 20:47:23 -0400

    updated active_record include

--- lib/has_many_polymorphic.rb
+++ lib/has_many_polymorphic.rb
@@ -1,5 +1,5 @@
 require 'has_many_polymorphic/has_many_polymorphic'
-ActiveRecord::Base.send :extend, RussellEdge::HasManyPolymorphic
+ActiveRecord::Base.send :include, RussellEdge::HasManyPolymorphic
 
 #used to tie into Rails initialization load the models so the relationships are created
 require 'has_many_polymorphic/autoload'
